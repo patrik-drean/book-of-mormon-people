59423982a2afa9841199d8c91e5ae554050e3dc0
Patrik <patrikdrean@gmail.com>
2019-10-02 14:41:41 -0600

    Updates

---
+++ precache-manifest.9c6f95dc00ba57b6ec80666a5364ceb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b4c74126a1d43f2ba0d0e907a9bb494",
+    "url": "/book-of-mormon-people/index.html"
+  },
+  {
+    "revision": "a8738c499665f604febb",
+    "url": "/book-of-mormon-people/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0154e44fcc31df9d7873",
+    "url": "/book-of-mormon-people/static/js/2.a37e0fc9.chunk.js"
+  },
+  {
+    "revision": "a8738c499665f604febb",
+    "url": "/book-of-mormon-people/static/js/main.5916e804.chunk.js"
+  },
+  {
+    "revision": "6450e0138e8a4d97c2c4",
+    "url": "/book-of-mormon-people/static/js/runtime~main.b9a8dd43.js"
+  }
+]);
