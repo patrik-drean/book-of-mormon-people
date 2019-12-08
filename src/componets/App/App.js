0442928d0eac6526b36086eba617df366fb23f83
Patrik <patrikdrean@gmail.com>
2019-12-08 14:27:02 -0700

    switched routing

--- src/componets/App/App.js
+++ src/componets/App/App.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import reducer from "../../reducers/reducers";
-import FlashCardsPage from "../FlashCardsPage/FlashCardsPage";
-import LearnPage from "../LearnPage/LearnPage";
-import Nav from "./Nav";
+import reducer from '../../reducers/reducers'
+import FlashCardsPage from '../FlashCardsPage/FlashCardsPage'
+import LearnPage from '../LearnPage/LearnPage'
+import Nav from './Nav'
 
 const appStyle = {
-  backgroundColor: "#282c34",
-  minHeight: "100vh",
-  display: "flex",
-  flexDirection: "column",
-  fontSize: "calc(10px + 2vmin)",
-  color: "white"
-};
+  backgroundColor: '#282c34',
+  minHeight: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  fontSize: 'calc(10px + 2vmin)',
+  color: 'white'
+}
 
-const store = createStore(reducer);
+const store = createStore(reducer)
 
 function App() {
   return (
@@ -26,10 +26,7 @@ function App() {
         <div className="App" style={appStyle}>
           <Nav />
           <Switch key={Math.random()}>
-            <Route
-              path="/book-of-mormon-people/learn"
-              component={LearnPage}
-            ></Route>
+            <Route path="/book-of-mormon-people/" component={LearnPage}></Route>
             <Route
               path="/book-of-mormon-people/quiz"
               component={FlashCardsPage}
@@ -38,7 +35,7 @@ function App() {
         </div>
       </Router>
     </Provider>
-  );
+  )
 }
 
-export default App;
+export default App
